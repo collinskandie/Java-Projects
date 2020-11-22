c5819434a1fff769543fe1f37624b00bdcfc44f8
Collins Kipruto <53892938+collinskandie@users.noreply.github.com>
2020-11-22 17:49:48 +0300

    multidimensional arrays

---
+++ Main.java
@@ -0,0 +1,22 @@
+package com.collinskandie;
+public class Main{
+
+public static void main(String args[]){
+// A simple constructor.
+               class MyClass {
+                int x;
+                MyClass() {
+                        x = 10;
+                }
+                // A method to set x
+                public void setX(int z) {
+                        x = z;
+                }
+                // A method to get x
+                public int getX() {
+                        System.out.println(x);
+                        return x;
+                }
+                }
+}
+}
